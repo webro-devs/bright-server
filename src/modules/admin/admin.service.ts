c000ae3dd808ffd96eec1d81b9c3d879079df3bb
jaloliddin05 <nasrullayevjaloliddin.ajf@gmail.com>
2023-03-10 11:12:04 +0500

    feat: added update method to admin

--- src/modules/admin/admin.service.ts
+++ src/modules/admin/admin.service.ts
@@ -1,10 +1,4 @@
-import {
-  UpdateResult,
-  DeleteResult,
-  Repository,
-  DataSource,
-  EntityManager,
-} from "typeorm";
+import { DeleteResult, Repository, DataSource, EntityManager } from "typeorm";
 import { hashPassword } from "../../infra/helpers";
 import { PermissionService } from "../permission/permission.service";
 import { Admin } from "./admin.entity";
@@ -55,27 +49,27 @@ export class AdminService {
     return admin;
   }
 
-  async update(values: UpdateAdminDto, id: string): Promise<UpdateResult> {
-    let password,
-      permissions,
-      admin = await this.getById(id);
-    permissions = admin.permissions;
-    password = admin.password;
-
+  async update(values: UpdateAdminDto, id: string): Promise<Admin> {
+    let admin = await this.getById(id);
+    admin.city = values.city ? values.city : admin.city;
+    admin.education = values.education ? values.education : admin.education;
+    admin.fullName = values.fullName ? values.fullName : admin.fullName;
+    admin.login = values.login ? values.login : admin.login;
+    admin.phone = values.phone ? values.phone : admin.phone;
     if (values.permissions) {
-      permissions = await this.permissionService.getManyPermissionsById(
+      const permissions = await this.permissionService.getManyPermissionsById(
         values.permissions,
       );
+      admin.permissions = permissions;
     }
     if (values.password) {
-      password = hashPassword(values.password);
+      const password = await hashPassword(values.password);
+      admin.password = password;
     }
-    const response = await this.adminRepository.update(id, {
-      ...values,
-      password,
-      permissions,
+    await this.connection.transaction(async (manager: EntityManager) => {
+      await manager.save(admin);
     });
-    return response;
+    return admin;
   }
 
   async changeActive(id: string, isActive: boolean) {
@@ -88,7 +82,7 @@ export class AdminService {
       admin = await this.getById(id);
     password = admin.password;
     if (values.password) {
-      password = hashPassword(values.password);
+      password = await hashPassword(values.password);
     }
     const response = await this.adminRepository.update(id, {
       ...values,
