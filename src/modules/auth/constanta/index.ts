4114b828f2ee4772804baee474af07f139e2ad6e
Muxammadaziz <muxammadazizramziddinov@gmail.com>
2023-03-14 09:46:18 +0500

    cors changet

--- src/modules/auth/constanta/index.ts
+++ src/modules/auth/constanta/index.ts
@@ -3,7 +3,7 @@ import { CookieOptions } from "express";
 export const ACCESS_TOKEN_ADMIN = "access_token_admin";
 export const REFRESH_TOKEN_ADMIN = "refresh_token_admin";
 export const accessTokenOptions: CookieOptions = {
-  secure: true,
+  // secure: true,
   sameSite: "none",
   maxAge: 31536000000,
 };
