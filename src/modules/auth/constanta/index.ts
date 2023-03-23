b74998dea7748fc7bc5a80bbcb6834649aa18c54
Muxammadaziz <muxammadazizramziddinov@gmail.com>
2023-03-23 11:43:23 +0500

    auth middle ware

--- src/modules/auth/constanta/index.ts
+++ src/modules/auth/constanta/index.ts
@@ -3,10 +3,10 @@ import { CookieOptions } from "express";
 export const ACCESS_TOKEN_ADMIN = "access_token_admin";
 export const REFRESH_TOKEN_ADMIN = "refresh_token_admin";
 export const accessTokenOptions: CookieOptions = {
-  secure: process.env.NODE_ENV === 'production',
+  secure: true,
   sameSite: "none",
   maxAge: 31536000000,
-  httpOnly: process.env.NODE_ENV === 'production',
+  httpOnly: true,
 };
 export const refreshTokenOptions: CookieOptions = {
   ...accessTokenOptions,
