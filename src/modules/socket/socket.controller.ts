320f6b1bf3ac12ce7d2d10a98c3c3da4d51e0a15
jaloliddin05 <nasrullayevjaloliddin.ajf@gmail.com>
2023-03-28 11:32:12 +0500

    ...

--- src/modules/socket/socket.controller.ts
+++ src/modules/socket/socket.controller.ts
@@ -4,46 +4,47 @@ import { socketService } from "./socket.module";
 // const Service = new NotificationService()
 
 export const OnJoin = async (data: OnJoinType, socket: any, io: any) => {
-    try {
-        console.log(`User id: ${data.id} ==== Socket id: ${socket.id}`);
-        await socketService.create({ socketId: socket.id, admin: data?.id })
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    console.log(`User id: ${data.id} ==== Socket id: ${socket.id}`);
+    await socketService.create({ socketId: socket.id, admin: data?.id });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const OnDisconnect = async (socket: any) => {
-    try {
-        await socketService.removeBySocketId(socket?.id)
-        socket.disconnect(true);
-        socket = null;
-    } catch (error) {
-        console.log(error);
-
-    }
-}
+  try {
+    await socketService.removeBySocketId(socket?.id);
+    socket.disconnect(true);
+    socket = null;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const OnCreate = (roomId: string, socket: any, io: any) => {
-    try {
-        socket.join(roomId);
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    socket.join(roomId);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-export const OnChange = (data: {roomId: string, inputName: string, value: string}, socket:any, io:any) => {
-    try {
-        io.sockets.in(data.roomId).emit('input_change', data);
-    } catch (error) {
-        console.log(error);    
-    }
-}
+export const OnChange = (
+  data: { roomId: string; inputName: string; value: string },
+  socket: any,
+  io: any,
+) => {
+  try {
+    io.sockets.in(data.roomId).emit("input_change", data);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const OnInputChange = async () => {
-    try {
-        
-    } catch (error) {
-        console.log(error);
-        
-    }
-}
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+};
