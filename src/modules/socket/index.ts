ce7891501238f5c1247b8695416611bfe1151513
Muxammadaziz <muxammadazizramziddinov@gmail.com>
2023-03-28 10:28:43 +0500

    sknjsbf

--- src/modules/socket/index.ts
+++ src/modules/socket/index.ts
@@ -1,5 +1,5 @@
 import { Server } from "socket.io";
-import { OnJoin } from "./socket.controller";
+import { OnCreate, OnDisconnect, OnJoin } from "./socket.controller";
 
 export const connectSocket = (server: any) => {
     const io = new Server(server);
@@ -7,9 +7,7 @@ export const connectSocket = (server: any) => {
     io.on("connection", (socket) => {
         console.log(`a user: ${socket.id} connected from Socket.io`);
         socket.on('join', (data) => OnJoin(data, socket, io))
-        socket.on('disconnect', (reason) => {
-            socket.disconnect(true);
-            socket = null;
-          });
+        socket.on('create', (roomId) => OnCreate(roomId, socket, io))
+        socket.on('disconnect', () => OnDisconnect(socket));
     });
 }
--- src/modules/socket/socket.controller.ts
+++ src/modules/socket/socket.controller.ts
@@ -6,13 +6,33 @@ import { socketService } from "./socket.module";
 export const OnJoin = async (data: OnJoinType, socket: any, io: any) => {
     try {
         console.log(`User id: ${data.id} ==== Socket id: ${socket.id}`);
-        await socketService.create({socketId: socket.id, admin: data?.id})
+        await socketService.create({ socketId: socket.id, admin: data?.id })
     } catch (error) {
         console.log(error);
     }
 }
 
-export const OnDisconnect = async () => {
+export const OnDisconnect = async (socket: any) => {
+    try {
+        await socketService.removeBySocketId(socket?.id)
+        socket.disconnect(true);
+        socket = null;
+    } catch (error) {
+        console.log(error);
+
+    }
+}
+
+export const OnCreate = (roomId: string, socket: any, io: any) => {
+    try {
+        socket.join(roomId);
+        io.sockets.in(roomId).emit('event', 'data');
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const OnInputChange = async () => {
     try {
         
     } catch (error) {
