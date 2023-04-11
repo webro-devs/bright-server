8a7cb8b5d9af8d006c41c592a2caf7ebb0287691
siddiqun03 <siddiqun.dev@gmail.com>
2023-04-11 14:20:44 +0500

    feat: status added

--- src/modules/chat-message/chat-message.controller.ts
+++ src/modules/chat-message/chat-message.controller.ts
@@ -20,7 +20,7 @@ export async function create(req: Request, res: Response) {
     const values: CreateMessageDto = req.body;
     values.user = req["user"].id;
     const response = await messageService.create(values);
-    res.send(response);
+    res.send({ message: "succesfully created" }).status(201);
   } catch (err) {
     throw new HttpException(true, 500, err.message);
   }
