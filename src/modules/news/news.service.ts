c53cb6a5b20f11325c98fe31c94c87e02407e29f
jaloliddin05 <nasrullayevjaloliddin.ajf@gmail.com>
2023-05-01 16:57:11 +0500

    ...

--- src/modules/news/news.service.ts
+++ src/modules/news/news.service.ts
@@ -311,9 +311,6 @@ export class NewsService {
         );
         oldNews.mainCategory = mainCategory;
       }
-      if (values?.publishDate) {
-        oldNews.publishDate = values.publishDate;
-      }
       if (values?.file) {
         if (oldNews?.file && values.file != oldNews?.file) {
           await fileService.removeFile(oldNews.file);
