06feb1fe813bab129474510608f237ae8db3988a
Getter IT Company <130207894+Getteruz@users.noreply.github.com>
2023-11-27 16:18:06 +0500

    Update currency.controller.ts

--- src/modules/currency/currency.controller.ts
+++ src/modules/currency/currency.controller.ts
@@ -8,13 +8,15 @@ export async function getCurrency(req: Request, res: Response) {
     await request(
       "http://cbu.uz/uzc/arkhiv-kursov-valyut/json/",
       function (error, response, body) {
-        if (!error && response.statusCode == 200) {
+        try{
+        if (!error && response.statusCode == 200 && body) {
           const data = JSON.parse(body).filter(
             (e) => e.Ccy == "USD" || e.Ccy == "EUR" || e.Ccy == "RUB",
           );
 
           res.send(data);
         }
+        } catch (err){}
       },
     );
   } catch (err) {
