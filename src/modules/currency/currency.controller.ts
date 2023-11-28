481cfeba4998200936d955f6ec0affbf4373ebe0
Getter IT Company <130207894+Getteruz@users.noreply.github.com>
2023-11-28 19:18:58 +0500

    Update currency.controller.ts

--- src/modules/currency/currency.controller.ts
+++ src/modules/currency/currency.controller.ts
@@ -5,20 +5,21 @@ import * as request from "request";
 
 export async function getCurrency(req: Request, res: Response) {
   try {
-    await request(
-      "http://cbu.uz/uzc/arkhiv-kursov-valyut/json/",
-      function (error, response, body) {
-        try{
-        if (!error && response.statusCode == 200 && body) {
-          const data = JSON.parse(body).filter(
-            (e) => e.Ccy == "USD" || e.Ccy == "EUR" || e.Ccy == "RUB",
-          );
+    // await request(
+    //   "http://cbu.uz/uzc/arkhiv-kursov-valyut/json/",
+    //   function (error, response, body) {
+    //     try{
+    //     if (!error && response.statusCode == 200 && body) {
+    //       const data = JSON.parse(body).filter(
+    //         (e) => e.Ccy == "USD" || e.Ccy == "EUR" || e.Ccy == "RUB",
+    //       );
 
-          res.send(data);
-        }
-        } catch (err){}
-      },
-    );
+    //       res.send(data);
+    //     }
+    //     } catch (err){}
+    //   },
+    // );
+    res.send({})
   } catch (err) {
     // throw new HttpException(true, 500, err.message);
     res.send({})
